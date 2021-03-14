b4c7063d8cee42721cccb32ca091534de231b1d9
Borovits <n.borovits@tue.nl>
2021-03-14 23:30:16 +0100

    inconsistency detection folder

---
+++ 6 Detect linguistic inconsistency/skipgram/lstm_word2vec.py
@@ -0,0 +1,284 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+import spacy 
+from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.optimizers import Adam,SGD,Adadelta, Adagrad
+
+import tensorflow as tf
+
+from keras.losses import MeanAbsoluteError
+
+from matplotlib import pyplot
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import matthews_corrcoef
+
+class lstm:
+    
+    def __init__(self, mutated_dst):
+        
+        self.mutated = mutated_dst
+        
+        self.train_set, self.val_set, self.test_set = np.split(self.mutated.sample(frac=1), 
+                                                [int(.6*len(self.mutated)), int(.8*len(self.mutated))])
+        
+        self.y_train = self.train_set['consistent'].astype(int)
+
+        self.y_test = self.test_set['consistent'].astype(int)
+
+        self.y_val = self.val_set['consistent'].astype(int)
+        
+    
+    def word2vecskip(self):
+        
+        tasks_train_w2v = [row for row in self.train_set['task_complete']]
+        vector_size = 100
+        w2v_model_tasks = Word2Vec(tasks_train_w2v, sg = 1, size = vector_size, window=6, min_count=1, workers=4, iter= 1000)
+        
+        return w2v_model_tasks, vector_size
+    
+    @staticmethod
+    def list_to_string(lst):
+        
+        one_string = ' '.join(lst)
+    
+        return one_string
+    
+    
+    def prepare(self):
+        
+        tokenizer_train = Tokenizer(lower=False) 
+        tokenizer_train.fit_on_texts(self.train_set['task_complete'])
+
+        tokenizer_test = Tokenizer(lower=False) 
+        tokenizer_test.fit_on_texts(self.test_set['task_complete'])
+
+        tokenizer_val = Tokenizer(lower=False) 
+        tokenizer_val.fit_on_texts(self.val_set['task_complete'])
+        
+        self.train_set['task_complete_one_string'] = self.train_set['task_complete'].apply(lambda x: self.list_to_string(x))
+        self.test_set['task_complete_one_string'] = self.test_set['task_complete'].apply(lambda x: self.list_to_string(x))
+        self.val_set['task_complete_one_string'] = self.val_set['task_complete'].apply(lambda x: self.list_to_string(x))
+        
+        tasks_train_tokens = tokenizer_train.texts_to_sequences(self.train_set['task_complete_one_string'])
+        tasks_test_tokens = tokenizer_test.texts_to_sequences(self.test_set['task_complete_one_string'])
+        tasks_val_tokens = tokenizer_val.texts_to_sequences(self.val_set['task_complete_one_string'])
+        
+        num_tokens = [len(tokens) for tokens in tasks_train_tokens]
+        num_tokens = np.array(num_tokens)
+        
+        max_tokens = np.mean(num_tokens) + 2 * np.std(num_tokens)
+        max_tokens = int(max_tokens)
+        
+        tasks_train_pad = pad_sequences(tasks_train_tokens, maxlen=max_tokens,padding='post')
+        tasks_test_pad = pad_sequences(tasks_test_tokens, maxlen=max_tokens,padding='post')
+        tasks_val_pad = pad_sequences(tasks_val_tokens, maxlen=max_tokens,padding='post')        
+        
+        return tasks_train_pad, tasks_test_pad, tasks_val_pad, max_tokens, tokenizer_train
+    
+    def create_emb_matrix(self,max_tokens,vector_size,w2v_model,tokenizertrain):
+        
+        embedding_dim = max_tokens
+        embedding_size = vector_size
+        num_words = len(tokenizertrain.word_index) + 1
+        embedding_matrix = np.random.uniform(-1, 1, (num_words, embedding_size))
+        
+        for word, i in tokenizertrain.word_index.items():
+            if i < num_words:
+                embedding_vector = w2v_model[word]
+            if embedding_vector is not None:
+                embedding_matrix[i] = embedding_vector
+                
+        return embedding_matrix, num_words
+    
+    def model_train(self,num_words,vector_size, embedding_matrix, max_tokens):
+        
+        tf.keras.backend.clear_session()
+        lstm_model = Sequential()
+
+        lstm_model.add(Embedding(input_dim = num_words,
+                            output_dim = vector_size,
+                            weights = [embedding_matrix],
+                            input_length = max_tokens,        
+                            trainable = False,              #the layer is not trained
+                            name='embedding_layer'))
+        lstm_model.add(LSTM(units = vector_size))
+        lstm_model.add(Dense(1, activation='sigmoid'))
+
+        lstm_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        
+        return lstm_model
+
+    def run_lstm(self):
+        
+        word2vec, emb_size = self.word2vecskip()
+        print('Word2Vec done')
+        print('==============')
+        
+        train_tasks_pad, test_tasks_pad, val_tasks_pad, tokens_max, train_tokenizer = self.prepare()
+        print('Prepare done')
+        print('==============')
+        
+        embed_matrix, num_of_words = self.create_emb_matrix(tokens_max, emb_size, word2vec, train_tokenizer)
+        print('Embedding matrix done')
+        print('==============')
+        
+        lstm_model = self.model_train(num_of_words, emb_size, embed_matrix, tokens_max)
+        print('ELSTM model declaration done')
+        print('==============')
+        
+        print('Train tasks pad shape: ', train_tasks_pad.shape)
+        print('Y Train shape: ', self.y_train.shape)
+        print('Test tasks pad shape: ', test_tasks_pad.shape)
+        print('Y Test shape: ', self.y_test.shape)
+
+        
+        histry = lstm_model.fit(train_tasks_pad, self.y_train, validation_data=(test_tasks_pad, self.y_test), epochs=20, 
+                                verbose = 0, batch_size=30)
+        
+        return histry, lstm_model, val_tasks_pad
+    
+    def viz_loss_acc(self,history,module='all'):
+        
+        pyplot.plot(history.history['accuracy'])
+        pyplot.plot(history.history['val_accuracy'])
+        pyplot.title('model accuracy')
+        pyplot.ylabel('accuracy')
+        pyplot.xlabel('epoch')
+        pyplot.legend(['train', 'test'], loc='upper left')
+        
+        path_acc = 'skip/lstm/lstm_word2vec_acc_' + module + '.jpg'
+        pyplot.savefig(path_acc)
+        pyplot.clf()
+        
+        pyplot.plot(history.history['loss'])
+        pyplot.plot(history.history['val_loss'])
+        pyplot.title('model loss')
+        pyplot.ylabel('loss')
+        pyplot.xlabel('epoch')
+        pyplot.legend(['train', 'test'], loc='upper left')
+        
+        path_loss = 'skip/lstm/lstm_word2vec_loss_' + module + '.jpg'
+        pyplot.savefig(path_loss)
+        pyplot.clf()
+        
+    def get_metrics(self,model_lstm,val_tasks_padded,module='all'):
+        
+        score = model_lstm.evaluate(val_tasks_padded, self.y_val, verbose=0)
+        print('Model loss:', score[0])
+        print('Validation accuracy:', score[1])
+        
+        y_pred = model_lstm.predict_classes(val_tasks_padded)
+        
+        matt = matthews_corrcoef(self.y_val, y_pred)
+        
+        cm = confusion_matrix(self.y_val, y_pred)
+        print(cm)
+        tp = cm[1][1]
+        fp = cm[0][1]
+        fn = cm[1][0]
+        tn= cm[0][0]
+        all_val = tp+fp+fn+tn
+        
+        precision = round(tp/(tp+fp),2)
+        recall = round(tp/(tp+fn), 2)
+        f1_score = (2*precision*recall)/(precision+recall)
+
+        precision_neg = round(tn/(tn+fn),2)
+        recall_neg = round(tn/(tn+fp), 2)
+        f1_score_neg = (2*precision_neg*recall_neg)/(precision_neg+recall_neg)
+        
+        ns_probs = [0 for _ in range(len(self.y_val))]
+        
+        ns_auc = roc_auc_score(self.y_val, ns_probs)
+        lr_auc = roc_auc_score(self.y_val, y_pred)
+        
+        print('Random choice: ROC AUC=%.3f' % (ns_auc))
+        print('Our model: ROC AUC=%.3f' % (lr_auc))
+        
+        ns_fpr, ns_tpr, _ = roc_curve(self.y_val, ns_probs)
+        lr_fpr, lr_tpr, _ = roc_curve(self.y_val, y_pred)
+        
+        pyplot.plot(ns_fpr, ns_tpr, linestyle='--', label='Random Choice')
+        pyplot.plot(lr_fpr, lr_tpr, marker='.', label='Our Model')
+        # axis labels
+        pyplot.xlabel('False Positive Rate')
+        pyplot.ylabel('True Positive Rate')
+        # show the legend
+        pyplot.legend()
+        path = '/skip/lstm/lstm_word2vec_roc_'+ module + '.jpg'
+        pyplot.savefig(path)
+        pyplot.clf()
+        
+        return matt, score[1], precision, recall, f1_score, precision_neg, recall_neg, f1_score_neg, lr_auc
+        
+        
+
+def get_filtered_mutated(mt,mod):
+    
+    mt = mt[mt['mod_keys_found_string'] == mod]
+    
+    return mt
+
+if __name__ == "__main__":
+    
+    with open('mutated.pkl', 'rb') as input_file:
+        mutated = pickle.load(input_file)   
+    
+    with open('top10_list.pkl', 'rb') as input_file:
+        top10_list = pickle.load(input_file)    
+    
+    metrics = ['acc',
+          'precision',
+          'recall',
+          'f1_score',
+          'precision_neg_cl',
+          'recall_neg_cl',
+          'f1_score_neg',
+          'matthews',
+          'roc_auc']
+        
+    top10_dict = dict((k,'') for k in top10_list)
+    results_df = pd.DataFrame(top10_dict, index = metrics)
+    
+    mutated['consistent'] = mutated['consistent'].astype(int)
+    
+    for module in top10_list:
+        
+        filtered_mutated = get_filtered_mutated(mutated, module)
+        
+        obj = lstm(filtered_mutated)
+
+        history, lstm_model, validation_tasks_pad = obj.run_lstm()
+
+        obj.viz_loss_acc(history,module)
+        print('Visualizations done')
+
+        matthews, accuracy, precision, recall, f1_score, precision_neg, recall_neg, f1_score_neg, roc_auc = obj.get_metrics(lstm_model,
+                                                                                                         validation_tasks_pad,
+                                                                                                         module)
+        print('Metrics calculated for module ', module)
+        print(matthews, accuracy, precision, recall, f1_score, precision_neg, recall_neg, f1_score_neg)
+        
+        mod_results = [matthews, accuracy, roc_auc, precision, recall, f1_score, precision_neg, recall_neg, f1_score_neg]
+          
+        results_df[module] = mod_results
+        print('Successfully put in results dataframe')
+        print('Next module calculation')
+        print('=========================')
+        
+    print(results_df)
+    results_df.to_excel('skip/lstm_word2vec.xlsx')
---
+++ 6 Detect linguistic inconsistency/xgboost_fastext.py
@@ -0,0 +1,212 @@
+import pandas as pd
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+
+import spacy 
+from gensim.models import FastText
+from gensim.models import KeyedVectors
+from sklearn import metrics
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, roc_curve
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import matthews_corrcoef
+from xgboost import XGBClassifier
+
+class xgboost:
+    
+    def __init__(self, mutated_dst):
+        
+        self.mutated = mutated_dst[['task_complete','consistent']]
+        
+        self.X = self.mutated['task_complete']
+        self.y = self.mutated['consistent']
+        
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
+        
+    def fastext(self):
+        
+        tasks_train_w2v = [row for row in self.X_train]
+        vector_size = 300
+        w2v_model_tasks = FastText(tasks_train_w2v, sg = 0, size = vector_size, window=6, min_count=1, workers=4, iter= 1000)
+        
+        return w2v_model_tasks, vector_size
+    
+    @staticmethod
+    def make_feature_vec(words, model, emb_size):
+        """
+        Average the word vectors for a set of words
+        """
+        feature_vec = np.zeros((emb_size,),dtype="float32")  # pre-initialize (for speed)
+        nwords = 0.
+        index2word_set = set(model.wv.index2word)  # words known to the model
+
+        for word in words:
+            if word in index2word_set: 
+                nwords = nwords + 1.
+                feature_vec = np.add(feature_vec,model[word])
+
+        nwords = np.array([nwords])
+
+        feature_vec = feature_vec / nwords[:,None]
+        return feature_vec    
+    
+    def get_avg_feature_vecs(self,tasks, model, emb_size):
+        """
+        Calculate average feature vectors for all tasks
+        """
+        counter = 0
+        tasks_feature_vecs = np.zeros((len(tasks),emb_size), dtype='float64')  # pre-initialize (for speed)
+
+        for task in tasks:
+            tasks_feature_vecs[counter] = self.make_feature_vec(task, model, emb_size)
+            counter = counter + 1
+        return tasks_feature_vecs
+    
+    def prepare_train_test_tasks(self, model_tasks, emb_size):
+        
+        clean_train_tasks = [task for task in self.X_train]
+
+        trainDataVecs = self.get_avg_feature_vecs(clean_train_tasks, model_tasks, emb_size)        
+        
+        clean_test_tasks = [task for task in self.X_test]
+
+        testDataVecs = self.get_avg_feature_vecs(clean_test_tasks, model_tasks, emb_size)
+        
+        return trainDataVecs,testDataVecs    
+    
+    def run_xgboost(self):
+        
+        fastext, emb_size = self.fastext()
+        
+        train_data_vecs, test_data_vecs = self.prepare_train_test_tasks(fastext, emb_size)
+        
+        param_grid = {'learning_rate': [0.05, 0.10, 0.30],
+         'max_depth': [10, 30, 50],
+         'min_child_weight': [1, 5, 7],
+#          'gamma': [ 0.0, 0.2, 0.4 ],
+#          'colsample_bytree': [0.3, 0.5, 0.7],
+#          'n_estimators': [50, 100, 150] 
+        }        
+        
+        xgb = XGBClassifier(n_estimators = 100, 
+                            objective='binary:logistic',
+                            gamma = 0.0,
+                            colsample_bytree=.7,
+                            silent=1)
+        
+        cv = StratifiedKFold(n_splits=10,
+                     shuffle=True,
+                     random_state=42)
+        
+        grid_search = GridSearchCV(estimator = xgb, param_grid = param_grid, 
+                          cv = cv, n_jobs = -1, verbose = 2)
+        
+        xgb_hyper = grid_search.fit(train_data_vecs, self.y_train)
+        
+        best_grid = grid_search.best_estimator_
+        
+        result = best_grid.predict(test_data_vecs)
+        
+        class_report = classification_report(self.y_test, result, output_dict = True)
+
+        fpr, tpr, _ = roc_curve(self.y_test, result)
+        auc = roc_auc_score(self.y_test, result)
+        
+        mat = matthews_corrcoef(self.y_test, result)
+        
+        return class_report, best_grid, fpr, tpr, auc, mat
+        
+
+
+def get_filtered_mutated(mt,mod):
+    
+    mt = mt[mt['mod_keys_found_string'] == mod]
+    
+    return mt
+
+def results_to_dict(results):
+    
+    end_dict = {}
+    end_dict['acc'] = round(results['accuracy'],3)
+    neg_keys = ['precision_neg_cl', 'recall_neg_cl', 'f1_score_neg']
+
+    mod = 'shell'
+    norm = results['0']
+    neg = results['1']
+    del neg['support']
+    del norm['support']
+
+    for key in norm.keys():
+        norm[key] = round(norm[key],3)
+        neg[key] = round(neg[key],3)
+
+    for key,n_key in zip(list(neg.keys()), neg_keys):
+        neg[n_key] = neg.pop(key)
+    z = {**norm, **neg}
+    end_dict = {**end_dict, **z}
+    
+    return end_dict
+
+if __name__ == "__main__":
+    
+    with open('mutated.pkl', 'rb') as input_file:
+        mutated = pickle.load(input_file)
+        
+    with open('top10_list.pkl', 'rb') as input_file:
+        top10_list = pickle.load(input_file)
+        
+    mutated['consistent'] = mutated['consistent'].astype(int)
+    
+    metrics = ['acc',
+          'precision',
+          'recall',
+          'f1-score',
+          'precision_neg_cl',
+          'recall_neg_cl',
+          'f1_score_neg',
+          'matthews',
+          'roc_auc']
+        
+    results_df = pd.DataFrame(index = metrics)
+    
+    for module in top10_list:
+        
+        print('Running XGBoost for the module ', module)
+        
+        filtered_mutated = get_filtered_mutated(mutated, module)
+    
+        rand_obj = xgboost(filtered_mutated)
+
+        report_dict, best_grid, fpr, tpr, auc, mat = rand_obj.run_xgboost()
+        
+        print('The best estimators for the module ', module, ' are: ', best_grid)
+        
+        file_title = 'xgb_best_params/xgb_fasttext_best_params_' + module + '.pkl'
+        with open(file_title,'wb') as output_file:
+            pickle.dump(best_grid, output_file)
+        
+        column_dict = results_to_dict(report_dict)
+        
+        column_dict['matthews'] = round(mat,3)
+        column_dict['roc_auc'] = round(auc,3)
+        
+        results_df[module] = results_df.index.to_series().map(column_dict)
+        
+        plt.figure(1)
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.plot(fpr, tpr, label='AUC {:.3f}'.format(auc))
+        plt.xlabel('False positive rate')
+        plt.ylabel('True positive rate')
+        plt.title('ROC curve')
+        plt.legend(loc='best')
+        path = 'xgb/xgb_fasttext_'+ module + '.jpg'
+        plt.savefig(path)
+        plt.clf()
+        
+        print('For module ', module, ' the results are ', column_dict)
+        print('=====================================')
+        
+    print(results_df)
+    results_df.to_excel('xgboost_fasttext.xlsx')
