af557db125ffa9765b119cd367e43e511bbf7956
IndikaKuma <ipkumarawd@yahoo.com>
2021-03-15 19:57:50 +0100

    Pushed the previous repository to the new repository

---
+++ 6 Detect linguistic inconsistency/fastText/skipgram/mlp_fasttext.py
@@ -0,0 +1,209 @@
+import pickle
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from gensim.models import FastText
+from scipy.stats import randint
+from sklearn.metrics import classification_report, roc_auc_score, roc_curve
+from sklearn.metrics import matthews_corrcoef
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+
+
+class mlp:
+
+    def __init__(self, mutated_dst):
+
+        self.mutated = mutated_dst[['task_complete', 'consistent']]
+
+        self.X = self.mutated['task_complete']
+        self.y = self.mutated['consistent']
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2,
+                                                                                random_state=42)
+
+    def fasttext(self):
+
+        tasks_train_w2v = [row for row in self.X_train]
+        vector_size = 100
+        w2v_model_tasks = FastText(tasks_train_w2v, sg=0, size=vector_size, window=6, min_count=1, workers=4, iter=1000)
+
+        return w2v_model_tasks, vector_size
+
+    @staticmethod
+    def make_feature_vec(words, model, emb_size):
+        """
+        Average the word vectors for a set of words
+        """
+        feature_vec = np.zeros((emb_size,), dtype="float32")  # pre-initialize (for speed)
+        nwords = 0.
+        index2word_set = set(model.wv.index2word)  # words known to the model
+
+        for word in words:
+            if word in index2word_set:
+                nwords = nwords + 1.
+                feature_vec = np.add(feature_vec, model[word])
+
+        nwords = np.array([nwords])
+
+        feature_vec = feature_vec / nwords[:, None]
+        return feature_vec
+
+    def get_avg_feature_vecs(self, tasks, model, emb_size):
+        """
+        Calculate average feature vectors for all tasks
+        """
+        counter = 0
+        tasks_feature_vecs = np.zeros((len(tasks), emb_size), dtype='float64')  # pre-initialize (for speed)
+
+        for task in tasks:
+            tasks_feature_vecs[counter] = self.make_feature_vec(task, model, emb_size)
+            counter = counter + 1
+        return tasks_feature_vecs
+
+    def prepare_train_test_tasks(self, model_tasks, emb_size):
+
+        clean_train_tasks = [task for task in self.X_train]
+
+        trainDataVecs = self.get_avg_feature_vecs(clean_train_tasks, model_tasks, emb_size)
+
+        clean_test_tasks = [task for task in self.X_test]
+
+        testDataVecs = self.get_avg_feature_vecs(clean_test_tasks, model_tasks, emb_size)
+
+        return trainDataVecs, testDataVecs
+
+    def run_mlp(self):
+
+        fasttext, emb_size = self.fasttext()
+
+        train_data_vecs, test_data_vecs = self.prepare_train_test_tasks(fasttext, emb_size)
+
+        parameter_space = {
+            'hidden_layer_sizes': [(randint.rvs(100, 600, 1), randint.rvs(100, 600, 1),),
+                                   (randint.rvs(100, 600, 1),)],
+            #             'activation': ['tanh', 'relu'],
+            'solver': ['sgd', 'adam'],
+            #             'alpha':  [0.0001, 0.05],
+            #             'learning_rate': ['constant','adaptive'],
+        }
+
+        mlp = MLPClassifier(max_iter=50, activation='relu', alpha=0.001, learning_rate='adaptive')
+
+        cv = StratifiedKFold(n_splits=10,
+                             shuffle=True,
+                             random_state=42)
+
+        grid_search = GridSearchCV(estimator=mlp, param_grid=parameter_space,
+                                   cv=cv, n_jobs=-1, verbose=2)
+
+        mlp_hyper = grid_search.fit(train_data_vecs, self.y_train)
+
+        best_grid = grid_search.best_estimator_
+
+        result = best_grid.predict(test_data_vecs)
+
+        class_report = classification_report(self.y_test, result, output_dict=True)
+
+        probs = best_grid.predict_proba(test_data_vecs)[:, 1]
+
+        fpr, tpr, _ = roc_curve(self.y_test, probs)
+        auc = roc_auc_score(self.y_test, probs)
+
+        mat = matthews_corrcoef(self.y_test, result)
+
+        return class_report, best_grid, fpr, tpr, auc, mat
+
+
+def get_filtered_mutated(mt, mod):
+    mt = mt[mt['mod_keys_found_string'] == mod]
+
+    return mt
+
+
+def results_to_dict(results):
+    end_dict = {}
+    end_dict['acc'] = round(results['accuracy'], 3)
+    neg_keys = ['precision_neg_cl', 'recall_neg_cl', 'f1_score_neg']
+
+    mod = 'shell'
+    norm = results['0']
+    neg = results['1']
+    del neg['support']
+    del norm['support']
+
+    for key in norm.keys():
+        norm[key] = round(norm[key], 3)
+        neg[key] = round(neg[key], 3)
+
+    for key, n_key in zip(list(neg.keys()), neg_keys):
+        neg[n_key] = neg.pop(key)
+    z = {**norm, **neg}
+    end_dict = {**end_dict, **z}
+
+    return end_dict
+
+
+if __name__ == "__main__":
+
+    with open('mutated.pkl', 'rb') as input_file:
+        mutated = pickle.load(input_file)
+
+    with open('top10_list.pkl', 'rb') as input_file:
+        top10_list = pickle.load(input_file)
+
+    mutated['consistent'] = mutated['consistent'].astype(int)
+
+    metrics = ['acc',
+               'precision',
+               'recall',
+               'f1-score',
+               'precision_neg_cl',
+               'recall_neg_cl',
+               'f1_score_neg',
+               'matthews',
+               'roc_auc']
+
+    results_df = pd.DataFrame(index=metrics)
+
+    for module in top10_list:
+        print('Running MLP for the module ', module)
+
+        filtered_mutated = get_filtered_mutated(mutated, module)
+
+        rand_obj = mlp(filtered_mutated)
+
+        report_dict, best_params, fpr, tpr, auc, mat = rand_obj.run_mlp()
+
+        print('The best estimators for the module ', module, ' are: ', best_params)
+
+        file_title = 'mlp_best_params/mlp_fasttext_best_params_' + module + '.pkl'
+        with open(file_title, 'wb') as output_file:
+            pickle.dump(best_params, output_file)
+
+        column_dict = results_to_dict(report_dict)
+
+        column_dict['matthews'] = round(mat, 3)
+        column_dict['roc_auc'] = round(auc, 3)
+
+        results_df[module] = results_df.index.to_series().map(column_dict)
+
+        plt.figure(1)
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.plot(fpr, tpr, label='AUC {:.3f}'.format(auc))
+        plt.xlabel('False positive rate')
+        plt.ylabel('True positive rate')
+        plt.title('ROC curve')
+        plt.legend(loc='best')
+        path = 'mlp/mlp_fastext_' + module + '.jpg'
+        plt.savefig(path)
+        plt.clf()
+
+        print('For module ', module, ' the results are ', column_dict)
+        print('=====================================')
+
+    print(results_df)
+    results_df.to_excel('mlp_fasttext.xlsx')
